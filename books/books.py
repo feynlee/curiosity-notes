9fc3d692f52d73cd7ec442d6513175e01bbc595b
Ziyue Li <liziyue241@gmail.com>
2022-10-12 20:15:59 -0400

    fix books path

--- books/books.py
+++ books/books.py
@@ -1,6 +1,6 @@
 import yaml
 
-with open('books.yml', 'r') as stream:
+with open('books/books.yml', 'r') as stream:
    books = yaml.load(stream, Loader=yaml.Loader)
 
 entries = [
@@ -74,5 +74,5 @@ f"""
 entries.append(":::::::")
 book_list = '\n'.join(entries)
 
-with open("books.md", "w") as f:
+with open("books/books.md", "w") as f:
     f.write(book_list)
